827d7e4c765432ef083e52ed5c3e4eff98e78461
TheCode327 <jpf32005@gmail.com>
2024-06-11 23:30:11 -0400

    makin mods with gaoy wowy

---
+++ src/main/java/net/TheCode327/learn_modding/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.TheCode327.learn_modding.item;
+
+import net.TheCode327.learn_modding.learnToMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, learnToMod.MOD_ID);
+
+    public static final RegistryObject<Item> ChoateDiploma = ITEMS.register("choateDiploma",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
--- src/main/java/net/TheCode327/learn_modding/learnToMod.java
+++ src/main/java/net/TheCode327/learn_modding/learnToMod.java
@@ -1,40 +1,36 @@
 package net.TheCode327.learn_modding;
 
 import com.mojang.logging.LogUtils;
+import net.TheCode327.learn_modding.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MOD_ID)
-public class ExampleMod
+
+@Mod(learnToMod.MOD_ID)
+public class learnToMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "learn_modding";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod()
-    {
+    public learnToMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
     }
@@ -44,10 +40,10 @@ public class ExampleMod
 
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ChoateDiploma);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
